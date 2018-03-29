83d097f4a784ea50aa43483747c9fb10abb8d484
Iza Dziura <izadziura@wp.pl>
2018-03-29 15:46:16 +0200

    It moves

--- js/script.js
+++ js/script.js
@@ -1,16 +1,15 @@
 $(function(){
 	//this code will execute after the DOM is loaded
-});
+	var carouselList = $('#carousel ul');
 
-var carouselList = $('carousel ul');
-
-$('#clickme').click(function() {
-	$('#carousel ul').animate({
-		'marginLeft': -500
-	}, 500, function moveFirstSlide() {
-		var firstItem = carouselList.find('li:first');
-		var lastItem = carouselList.find('li:last');
-		lastItem.after(firstItem)
-		carouselList.css({marginLeft:0});
+	$('#clickme').click(function() {
+		$(carouselList).animate({
+			'marginLeft': -500
+		}, 500, function moveFirstSlide() {
+			var firstItem = carouselList.find('li:first');
+			var lastItem = carouselList.find('li:last');
+			lastItem.after(firstItem)
+			carouselList.css({marginLeft:0});
+		});
 	});
 });
